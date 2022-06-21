b6738473afc321dd2a675aa719a0b8106389aca8
unknown <souravbose95@gmail.com>
2022-06-21 09:34:42 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+dotenv.config();
+connectDB();
+const app = express();
+
+// To accept JSON data
+app.use(express.json());
+
+app.use("/user", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on Port ${PORT}`);
+});
